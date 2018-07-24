131f8b276f5592466359ee4c2ddd0c24155f37d9
bitcodin <tigraine@tigraine.at>
2018-07-24 11:25:59 +0200

    finishing implementing updateVersion function

--- bitmovin-build-tools.sh
+++ bitmovin-build-tools.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 if [[ -z $1 ]]; then 
+	echo ""
+	echo "===================================================================="
 	echo "Please type below command in order to load bitmovin build tools:"
 	echo "	 source ./bitmovin-build-tools.sh"
 	echo ""
@@ -9,14 +11,16 @@ if [[ -z $1 ]]; then
 	echo "	package"
 	echo "	createDockerContainer"
 	echo "	pushContainer"
+	echo "===================================================================="
+	echo ""
 fi
 
 function updateVersion() {
 	#getting version from git
 	VERSION=$(git describe)
+	#remove prefix and suffiix so version follow x.y.z conention
 	VERSION=${VERSION#"v"}
-
-	echo $VERSION
+	VERSION=${VERSION%%-*}
 	
 	#updating version in pom.xml
 	sed -i "0,/<version>/{s/<version>.*/<version>$VERSION<version\/>/}" pom.xml
