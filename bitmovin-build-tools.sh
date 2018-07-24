899ac3f9060ce1d222fb00aea00109248f78e132
bitcodin <tigraine@tigraine.at>
2018-07-24 12:06:15 +0200

    implementing package function

--- bitmovin-build-tools.sh
+++ bitmovin-build-tools.sh
@@ -7,15 +7,15 @@ if [[ -z $1 ]]; then
 	echo "	 source ./bitmovin-build-tools.sh"
 	echo ""
 	echo "Availabe bitmovin functions: "
-	echo "	updateVersion"
-	echo "	package"
-	echo "	createDockerContainer"
-	echo "	pushContainer"
+	echo "	bm-update-version"
+	echo "	bm-package"
+	echo "	bm-create-docker"
+	echo "	bm-create-docker"
 	echo "===================================================================="
 	echo ""
 fi
 
-function updateVersion() {
+function bit_movin_update_version() {
 	#getting version from git
 	VERSION=$(git describe)
 	#remove prefix and suffiix so version follow x.y.z conention
@@ -23,5 +23,22 @@ function updateVersion() {
 	VERSION=${VERSION%%-*}
 	
 	#updating version in pom.xml
-	sed -i "0,/<version>/{s/<version>.*/<version>$VERSION<version\/>/}" pom.xml
+	sed -i "0,/<version>/{s/<version>.*/<version>$VERSION<\/version>/}" pom.xml
 }
+
+function bit_movin_package() {
+	mvn clean package
+}
+
+function bit_movin_create_docker() {
+	echo "TBC"
+}
+
+function bit_movin_push_container() {
+	echo "TBC"
+}
+
+alias bm-update-version=bit_movin_update_version
+alias bm-package=bit_movin_package
+alias bm-create-docker=bit_movin_create_docker
+alias bm-create-docker=bit_movin_push_container
